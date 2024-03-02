6bf7cb71b14f58beeff3856959172ae6e2e02233
Andreas Jonsson <ajdev8@gmail.com>
2024-03-02 20:45:33 +0100

    Add dasel for emacs-pet

--- install.sh
+++ install.sh
@@ -40,6 +40,10 @@ sudo apt install wslu
 sudo apt install cmake
 sudo apt install libvterm-dev
 
+# dasel (for emacs-pet)
+curl -sSLf "$(curl -sSLf https://api.github.com/repos/tomwright/dasel/releases/latest | grep browser_download_url | grep linux_amd64 | grep -v .gz | cut -d\" -f 4)" -L -o dasel && chmod +x dasel
+mv ./dasel $HOME/local/bin/dasel
+
 ## LaTeX
 
 # sudo apt install texlive-latex-recommended
@@ -59,6 +63,7 @@ sudo apt install rename
 # sudo apt install sshpass
 sudo apt install tree
 
+# ocrmypdf
 sudo apt install ocrmypdf
 sudo apt install tesseract-ocr-swe
 
