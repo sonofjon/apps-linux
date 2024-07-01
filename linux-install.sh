bd9ced70b78751cd77fcd326dc3bfc11a5fe22bd
Andreas Jonsson <ajdev8@gmail.com>
2024-07-01 13:55:57 +0200

    Major revision
    Add array and function to represent packages and execute installations.
    Use lower case for non-environment variables.

--- linux-install.sh
+++ linux-install.sh
@@ -3,125 +3,110 @@
 # Detect the OS and set the package manager command
 if [ -f /etc/debian_version ]; then
   echo "Detected Debian-based system (Ubuntu)"
-  PKG_MANAGER="apt"
+  pkg_manager="apt"
   sudo apt update && sudo apt upgrade
-elif [ -f /etc/fedora-release ]; then
+elif [ -f /etc/fedora-release ] || [ -L /etc/fedora-release ] ; then
   echo "Detected Fedora-based system"
-  PKG_MANAGER="dnf"
+  pkg_manager="dnf"
   sudo dnf upgrade
 else
   echo "Unsupported OS"
   exit 1
 fi
 
-### Applications
+declare -A PKG_MAP
+PKG_MAP=(
+    # Emacs - general:
+    [xclip]="xclip"
+    # Emacs - spelling:
+    [aspell]="aspell"
+    [aspell-en]="aspell-en"
+    [aspell-sv]="aspell-sv"
+    [hunspell]="hunspell"
+    [hunspell-sv]="hunspell-sv"
+    [wamerican]="deb:wamerican rpm:words"
+    # Emacs - vterm:
+    [wslu]="deb:wslu"  # no fedora package
+    [cmake]="cmake"
+    [libvterm]="deb:libvterm-dev rpm:libvterm-devel"
+    # LaTeX:
+    # [texlive-latex-recommended]="deb:texlive-latex-recommended"
+    # [texlive-latex-extra]="deb:texlive-latex-extra"
+    # [texlive-fonts-extra]="deb:texlive-fonts-extra"
+    # [texlive-lang-european]="deb:texlive-lang-european"
+    # [texlive-luatex]="deb:texlive-luatex"
+    # Utilities:
+    # [ansible]="ansible"
+    [bat]="bat"
+    [btop]="btop"
+    [fzf]="fzf"
+    [gdu]="gdu"
+    [markdown]="deb:markdown rpm:perl-Text-Markdown"
+    [mmv]="mmv"
+    [ncdu]="ncdu"
+    [rename]="deb:rename rpm:prename"
+    [ripgrep]="ripgrep"
+    # [sshpass]="sshpass"
+    [wakeonlan]="deb:wakeonlan rpm:wol"
+    # ocrmypdf:
+    [ocrmypdf]="ocrmypdf"
+    [tesseract]="deb:tesseract-ocr-swe rpm:tesseract-langpack-swe"
+    # GUI tools:
+    # [meld]="meld"
+    # Development - Node:
+    # [npm]="deb:npm rpm:nodejs.npm"
+    # Development - Python:
+    [python3-pip]="python3-pip"
+    [python3-venv]="deb:python3-venv"
+    # Linters:
+    [shellcheck]="shellcheck"    # Bash
+    [python3-demjson]="python3-demjson"   # JSON
+    # [chktex]="chktex"   # LaTex  ????
+    # sudo $pkg_manager install cargo
+    # cargo install --locked --git https://github.com/latex-lsp/texlab.git
+    [lua5.4]="apt:lua5.4"      # Lua
+    [luarocks]="luarocks"      # luarocks config lua_version 5.4
+    [python3-demjson]="python3-demjson"
+    [shellcheck]="shellcheck"
+    [libxml2]="deb:libxml2-utils rpm:libxml2"
+    # System:
+    [plocate]="plocate"
+    # [anacron]="anacron"
+    # [sqlite3]="sqlite3"
+    # Networking:
+    # [nmap]="nmap"
+    # [net-tools]="net-tools"  # arp, ifconfig, netstat, rarp, nameif and route
+    [traceroute]="traceroute"
+    # [traceroute]="inetutils-traceroute"
+)
 
-## Emacs
+install_packages() {
+  for package in "${!PKG_MAP[@]}"; do
+    deb_pkg=""; rpm_pkg=""
+    for value in ${PKG_MAP[$package]}; do
+      if [[ $value == deb:* ]]; then
+        deb_pkg=${value#deb:}
+      elif [[ $value == rpm:* ]]; then
+        rpm_pkg=${value#rpm:}
+      else
+        deb_pkg=$value
+        rpm_pkg=$value
+      fi
+    done
 
-# General
-sudo $PKG_MANAGER install xclip
+    if [ "$pkg_manager" = "apt" ] && [ -n "$deb_pkg" ]; then
+      sudo apt install $deb_pkg
+    elif [ "$pkg_manager" = "dnf" ] && [ -n "$rpm_pkg" ]; then
+      sudo dnf install $rpm_pkg
+    fi
+  done
+}
 
-# Spelling
-sudo $PKG_MANAGER install aspell aspell-en aspell-sv
-sudo $PKG_MANAGER install hunspell hunspell-sv
-sudo $PKG_MANAGER install wamerican # no fedora package
-sudo $PKG_MANAGER install wslu # no fedora package
-
-# vterm
-sudo $PKG_MANAGER install cmake
-sudo $PKG_MANAGER install libvterm-dev # no fedora package
+install_packages
 
 # dasel (for emacs-pet)
 # curl -sSLf "$(curl -sSLf https://api.github.com/repos/tomwright/dasel/releases/latest | grep browser_download_url | grep linux_amd64 | grep -v .gz | cut -d\" -f 4)" -L -o dasel && chmod +x dasel
 # mv ./dasel $HOME/local/bin/dasel
-
-## LaTeX
-
-# sudo $PKG_MANAGER install texlive-latex-recommended
-# sudo $PKG_MANAGER install texlive-latex-extra
-# sudo $PKG_MANAGER install texlive-fonts-extra
-# sudo $PKG_MANAGER install texlive-lang-european
-# sudo $PKG_MANAGER install texlive-luatex
-# tlmgr stuff
-
-## Terminal utilities
-
-# sudo $PKG_MANAGER install ansible
-sudo $PKG_MANAGER install bat
-sudo $PKG_MANAGER install btop
-sudo $PKG_MANAGER install fzf
-sudo $PKG_MANAGER install gdu # no fedora package
-sudo $PKG_MANAGER install markdown # no fedora package
-sudo $PKG_MANAGER install mmv
-sudo $PKG_MANAGER install ncdu
-sudo $PKG_MANAGER install rename # no fedora package
-sudo $PKG_MANAGER install ripgrep
-# sudo $PKG_MANAGER install sshpass
-sudo $PKG_MANAGER install tree
-sudo $PKG_MANAGER install wakeonlan # no fedora package
-
-# ocrmypdf
-sudo $PKG_MANAGER install ocrmypdf
-sudo $PKG_MANAGER install tesseract-ocr-swe # no fedora package
-
-## GUI tools
-
-# sudo $PKG_MANAGER install meld
-
-### Development
-
-## Node
-# sudo $PKG_MANAGER install npm
-
-## Python
-# sudo $PKG_MANAGER install python3
-sudo $PKG_MANAGER install python3-pip
-sudo $PKG_MANAGER install python3-venv # no fedora package
-
-## Linters
-
-# Bash
-
-sudo $PKG_MANAGER install shellcheck
-
-# JSON
-
-sudo $PKG_MANAGER install python3-demjson
-
-# LaTex
-
-# sudo $PKG_MANAGER install chktex
-
-# sudo $PKG_MANAGER install cargo
-# cargo install --locked --git https://github.com/latex-lsp/texlab.git
-
-# Lua
-
-# sudo $PKG_MANAGER install lua5.4
-# sudo $PKG_MANAGER install luarocks
-# luarocks config lua_version 5.4
-
-# Python
-#   See setup-python repo
-
-# XML
-
-sudo $PKG_MANAGER install libxml2-utils   # xmllint # no fedora package
-
-### System
-
-## General
-
-sudo $PKG_MANAGER install plocate
-# sudo $PKG_MANAGER install anacron
-# sudo $PKG_MANAGER install sqlite3
-
-## Networking
-
-sudo $PKG_MANAGER install nmap
-# sudo $PKG_MANAGER install net-tools   # arp, ifconfig, netstat, rarp, nameif and route
-sudo $PKG_MANAGER install traceroute
-# sudo $PKG_MANAGER install inetutils-traceroute
 
 ### Distro specific
 
@@ -142,4 +127,3 @@ if [ -f /etc/debian_version ]; then
     # App
     sudo snap install emacs --classic
 fi
-
