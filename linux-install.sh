c530890d73d85c445c061f79ed49964d720d09bb
Andreas Jonsson <ajdev8@gmail.com>
2025-05-03 19:36:58 +0200

    linux-install.sh: Disable dasel and ocrmypdf

--- linux-install.sh
+++ linux-install.sh
@@ -41,8 +41,8 @@ sudo apt install cmake
 sudo apt install libvterm-dev
 
 # dasel (for emacs-pet)
-curl -sSLf "$(curl -sSLf https://api.github.com/repos/tomwright/dasel/releases/latest | grep browser_download_url | grep linux_amd64 | grep -v .gz | cut -d\" -f 4)" -L -o dasel && chmod +x dasel
-mv ./dasel $HOME/local/bin/dasel
+# curl -sSLf "$(curl -sSLf https://api.github.com/repos/tomwright/dasel/releases/latest | grep browser_download_url | grep linux_amd64 | grep -v .gz | cut -d\" -f 4)" -L -o dasel && chmod +x dasel
+# mv ./dasel $HOME/local/bin/dasel
 
 ## LaTeX
 
@@ -71,8 +71,8 @@ sudo apt install tree
 sudo apt install wakeonlan
 
 # ocrmypdf
-sudo apt install ocrmypdf
-sudo apt install tesseract-ocr-swe
+# sudo apt install ocrmypdf
+# sudo apt install tesseract-ocr-swe
 
 ## GUI tools
 
